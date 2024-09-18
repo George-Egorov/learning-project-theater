c4c5527e34e658fb4d03f8197da5f8afefaba386
George <egorovgeorgi777@gmail.com>
2024-09-18 15:29:50 +0300

    second-commit

--- Script-js/script.js
+++ Script-js/script.js
@@ -1,29 +1,29 @@
 
 // Slider
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
+  // Optional parameters
 
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
 // Pop-up
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Найти поп-ап элемент
   const popup = document.querySelector(".pop-up-covid");
   // Найти крестик для закрытия поп-апа
@@ -33,8 +33,72 @@ document.addEventListener("DOMContentLoaded", function() {
   popup.style.display = "grid";
 
   // Добавить событие клика для крестика, чтобы закрыть поп-ап
-  closeBtn.addEventListener("click", function() {
-      popup.style.display = "none";
+  closeBtn.addEventListener("click", function () {
+    popup.style.display = "none";
   });
 });
+
+
+// Burger-menu
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Найти элементы кнопки открытия и кнопки закрытия
+//   const burgerOpen = document.querySelector(".header__menu-burger");
+//   const burgerClose = document.querySelector(".header__nav-burger-cross");
+//   const menu = document.querySelector(".header__nav-list-burger");
+
+//   // Открытие меню при нажатии на кнопку открытия
+//   burgerOpen.addEventListener("click", function() {
+//       menu.classList.add("active");
+//   });
+
+//   // Закрытие меню при нажатии на кнопку закрытия
+//   burgerClose.addEventListener("click", function() {
+//       menu.classList.remove("active");
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+  burgerOpen();
+  burgerClose();
+})
+
+function burgerOpen() {
+  const button = document.querySelector('.header__menu-burger');
+  const burgerMenuContainer = document.querySelector('.header__burger-background');
+  const body = document.querySelector('body');
+
+  if (button) {
+    button.addEventListener('click', function () {
+      if (burgerMenuContainer) {
+        burgerMenuContainer.classList.add('visible');
+        body.classList.add('hiden')
+      }
+    })
+  }
+}
+
+function burgerClose() {
+
+  const buttonClose = document.querySelector('.header__nav-burger-cross');
+  const burgerMenuContainer = document.querySelector('.header__burger-background');
+  const body = document.querySelector('body');
+
+  if (buttonClose) {
+    buttonClose.addEventListener('click', function () {
+      if (burgerMenuContainer) {
+        burgerMenuContainer.classList.remove('visible');
+        body.classList.remove('hiden');
+      }
+    })
+  }
+
+  if (burgerMenuContainer) {
+    burgerMenuContainer.addEventListener('click', function () {
+      if (burgerMenuContainer) {
+        burgerMenuContainer.classList.remove('visible');
+        body.classList.remove('hiden');
+      }
+    })
+  }
+}
 
